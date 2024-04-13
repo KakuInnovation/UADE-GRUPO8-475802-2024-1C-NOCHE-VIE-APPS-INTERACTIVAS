40374fac25fd691a71fcf2c193c019d93abf0087
Alejandro Guerrero <guerrero.alejandro@gmail.com>
2024-04-12 21:49:37 -0300

    actualización proyecto para VITE

--- modules/static.js
+++
@@ -1,3 +0,0 @@
-const { static } = require('express');
-const { join } = require('path');
-module.exports = folder => static(join(__dirname, folder))
--- routes/main.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-router.get('/', (req, res) => {
-    //res.sendFile(__dirname + '/views/home.html'); 
-    res.render("home.html");
-})
-
-module.exports = router
