7f51433ae6ece44a876df27ca5aa0349767e291d
Alejandro Guerrero <guerrero.alejandro@gmail.com>
2024-05-02 01:31:18 -0300

    Cambios home

--- ECommerce-Grupo8/src/pages/home.jsx
+++ ECommerce-Grupo8/src/pages/home.jsx
@@ -2,55 +2,52 @@ import React, { useState, useEffect } from 'react';
 import { Container, Grid, Card, CardMedia, CardContent, Typography } from '@mui/material';
 
 const Home = () => {
-  const [products, setProducts] = useState({
-    id: "",
-    titulo: "",
-    detalle: "",
-    precio: "",
-    categoria: "",
-    foto: "",
-    cantidad: "",
-  });
+  const [productos, setProductos] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/productos')
+    //fetch("http://localhost:3000/products")
+    fetch("db.json")
       .then(response => response.json())
-      .then(data => setProducts(data.products))
-      .catch(error => console.error('Error fetching products:', error));
+      .then(data => setProductos(data.productos))
+      .catch(error => console.error('Error fetching productos:', error));
   }, []);
 
   return (
     <Container>
       <Grid container spacing={3}>
-        {products.map(product => (
-          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Card>
-              <CardMedia
-                component="img"
-                height="200"
-                image={product.foto}
-                alt={product.titulo}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {product.titulo}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Detalle: {product.detalle}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Precio: {product.precio}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Categoría: {product.categoria}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  Cantidad disponible: {product.cantidad}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+        {productos && productos.length > 0 ? (
+          productos.map(producto => (
+            <Grid item key={producto.id} xs={12} sm={6} md={4} lg={3}>
+              <Card>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image={producto.foto}
+                  alt={producto.titulo}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {producto.titulo}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Detalle: {producto.detalle}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Precio: $ {producto.precio}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Categoría: {producto.categoria}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Cantidad disponible: {producto.cantidad}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Typography variant="body1">Cargando productos...</Typography>
+        )}
       </Grid>
     </Container>
   );
