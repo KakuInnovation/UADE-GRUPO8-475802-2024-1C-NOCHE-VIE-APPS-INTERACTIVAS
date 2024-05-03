d19f2f615b3154e5aea9c37c87960f6c3976968d
Alejandro Guerrero <guerrero.alejandro@gmail.com>
2024-05-03 17:35:49 -0300

    Carusel

--- ECommerce-Grupo8/src/main.jsx
+++ ECommerce-Grupo8/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {createTheme, CssBaseline, ThemeProvider} from "@mui/material";
 import {BrowserRouter} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const darkTheme = createTheme({
