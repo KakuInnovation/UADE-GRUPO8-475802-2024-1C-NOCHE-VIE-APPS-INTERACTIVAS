0104ddb7d03d5d131320539ddb793fd7a915f857
Agustin <aggarcia997@gmail.com>
2024-05-20 20:47:49 -0300

    se agrego un compenente para cargar imagenes desde la pc

--- ECommerce-Grupo8/src/components/perfil/CreateListing.jsx
+++ ECommerce-Grupo8/src/components/perfil/CreateListing.jsx
@@ -1,62 +1,111 @@
-import {Box, Dialog, DialogActions, DialogContentText, Grid, Select, TextField, Typography} from "@mui/material";
+import React from 'react';
+import {
+    Box, Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    Grid,
+    TextField,
+    Typography
+} from "@mui/material";
 import SelectPhoto from "./comps/SelectPhoto.jsx";
+import ComboBox from "./comps/ComboBox.jsx";
 
 const CreateListing = () => {
+    const sx_dialog = {
+        layout: {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+        },
+        dialogContext: {
+            display: 'flex',
+            justifyContent: 'start',
+            flexDirection: 'column',
+            backgroundColor:'pink',
+
+
+        },
+        dialogContentText: {
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            color: 'white',
+            backgroundColor: 'black',
+        },
+        formControl: {
+
+            backgroundColor: "white",
+        }
+    };
 
     return (
-        <Dialog open={true}>
-            <DialogContentText>
-                <Box>
-                    <Typography variant="h4">Crea tu publicacion!</Typography>
-                </Box>
-            </DialogContentText>
-            <DialogActions>
-                <Grid Container>
-                    <Grid item>
-                        <TextField
-                            defaultValue="Titulo"
-                            size={"small"}
-                            label="Titulo"
-                        />
-
-
-                    </Grid>
-                    <Grid item>
-                        <Typography>Buscar producto</Typography>
-                    </Grid>
-                    <Grid item>
-                        <SelectPhoto></SelectPhoto>
-                    </Grid>
-                    <Grid item>
-                        <Box>
-                            <Box>
-                                <Typography>Precio:</Typography>
-                                <TextField/>
-                            </Box>
-                            <Box>
-                                <Typography>Stock:</Typography>
-                                <TextField/>
-                            </Box>
-                        </Box>
-                    </Grid>
-                    <Grid item>
-                        <Box>
-                            <Box>
-                                <Box>
-                                    <Typography>Categoria: </Typography>
-                                    <Typography>Cantidad de jugadores: </Typography>
+        <Dialog open={true} sx={{...sx_dialog.layout}} maxWidth='lg' fullWidth>
+            <DialogContent sx={{...sx_dialog.dialogContext}}>
+                <DialogContentText sx={{...sx_dialog.dialogContentText}}>
+                    <Typography variant="h4" sx={{ textAlign: 'center', }}>Crea tu publicación!</Typography>
+                </DialogContentText>
+                <Box component="form" sx={{...sx_dialog, width: '100%' }}>
+                    <Grid container >
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <TextField
+                                fullWidth
+                                label="Titulo"
+                                size="small"
+                            />
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Typography>Buscar producto</Typography>
+                            <ComboBox />
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <SelectPhoto />
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Box sx={{...sx_dialog, display: 'flex', justifyContent: 'space-between', gap: 2 }}>
+                                <Box sx={{...sx_dialog, flex: 1 }}>
+                                    <Typography>Precio:</Typography>
+                                    <TextField fullWidth size="small" />
+                                </Box>
+                                <Box sx={{...sx_dialog, flex: 1 }}>
+                                    <Typography>Stock:</Typography>
+                                    <TextField fullWidth size="small" />
                                 </Box>
                             </Box>
-                            <Box>
-                                <Typography>Descripcion:</Typography>
-                                <TextField/>
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Box sx={{...sx_dialog}}>
+                                <Typography>Categoria:</Typography>
+                                <ComboBox></ComboBox>
                             </Box>
-                        </Box>
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Box sx={{...sx_dialog}}>
+                                <Typography>Descripcion del producto:</Typography>
+                                <ComboBox></ComboBox>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Box sx={{...sx_dialog}}>
+                                <Typography>Cantidad de jugadores:</Typography>
+                               <ComboBox></ComboBox>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={12} sx={sx_dialog.formControl}>
+                            <Typography>Descripción:</Typography>
+                            <TextField fullWidth multiline rows={4} />
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Box>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={() => console.log('Submit')}>Submit</Button>
+                <Button onClick={() => console.log('Cancel')}>Cancel</Button>
             </DialogActions>
         </Dialog>
-    )
+    );
+};
 
-}
 export default CreateListing;
