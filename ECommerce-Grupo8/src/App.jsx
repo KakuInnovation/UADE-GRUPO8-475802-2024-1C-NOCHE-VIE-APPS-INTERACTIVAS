1716ab5dcb3bfc5554ffbcdba6014ec3f78bd41e
Agustin <aggarcia997@gmail.com>
2024-06-27 17:13:51 -0300

    Se agrego la ruta privada (profile)

--- ECommerce-Grupo8/src/App.jsx
+++ ECommerce-Grupo8/src/App.jsx
@@ -14,9 +14,12 @@ import Perfil from "./pages/Perfil.jsx";
 import CreateListingDialog from "./components/perfil/comps/CreateListingDialog.jsx";
 import SignInSide from "./pages/SignInSide.jsx";
 import SignUp from "./pages/SignUp.jsx";
-import Home from "./pages/Home.jsx";
-import {Provider} from "react-redux";
+
+import {Provider, useSelector} from "react-redux";
 import store from './redux/store.js'
+import Home from "./pages/home.jsx";
+import ProtectedRouteProfile from "./protected_routes/PrivateRoute.jsx";
+
 
 function App() {
     const background =   'radial-gradient(circle at 50% -20.71%, #cbe7e1 0, #cae7e3 6.25%, #c9e7e6 12.5%, #c9e7e8 18.75%, #c9e7eb 25%, #c9e7ed 31.25%, #cae6ef 37.5%, #cbe6f1 43.75%, #cde5f2 50%, #cfe4f3 56.25%, #d1e4f4 62.5%, #d4e3f5 68.75%, #d7e2f5 75%, #dae1f5 81.25%, #dde0f4 87.5%, #e0e0f4 93.75%, #e3dff2 100%)';
@@ -29,13 +32,11 @@ function App() {
 
                 <Routes>
                     <Route path="/" element={<Home/>} />
-                    <Route path="/profile" element={<Perfil/>} />
+                    <Route path={'/profile'} element={<ProtectedRouteProfile><Perfil></Perfil></ProtectedRouteProfile>}/>
                     <Route path="/catalogo" element={<Catalogo/>} />
                     <Route path="/catalogo/:id" element={<Catalogo/>} />
                     <Route path="/login" element={<SignInSide/>} />
                     <Route path="/signup" element={<SignUp/>} />
-                    <Route path="/publicar" element={<Publicar />} />
-                    <Route path="/editar" element={<Editar />} />
                     <Route path="/payment" element={<Payment/>} />
                     <Route path="/producto/:id" element={<Producto/>} />
                     <Route path="/crear-publicacion" element={<CreateListingDialog/>} />
