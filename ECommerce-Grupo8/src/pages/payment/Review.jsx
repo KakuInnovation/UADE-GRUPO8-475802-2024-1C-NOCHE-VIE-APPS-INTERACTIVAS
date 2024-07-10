84606e513e142cbbc23e735cbd271881c35155f2
Agustin <aggarcia997@gmail.com>
2024-07-10 14:01:15 -0300

    Agregado de Review para el la logica de Payment

--- ECommerce-Grupo8/src/pages/payment/Review.jsx
+++ ECommerce-Grupo8/src/pages/payment/Review.jsx
@@ -4,6 +4,7 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
+import {useSelector} from "react-redux";
 
 const products = [
     {
@@ -37,34 +38,32 @@ const payments = [
     { name: 'Expiry date', detail: '04/2024' },
 ];
 
+
+
 export default function Review() {
+    const shoppingCart = useSelector (state => state.shopping_cart.shoppingCart);
+    const subtotal = useSelector(state => state.shopping_cart.subtotal);
+    const userId = useSelector(state => state.auth.userId);
     return (
         <React.Fragment>
             <Typography variant="h6" gutterBottom>
                 Order summary
             </Typography>
             <List disablePadding>
-                {products.map((product) => (
-                    <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
-                        <ListItemText primary={product.name} secondary={product.desc} />
-                        <Typography variant="body2">{product.price}</Typography>
+                {shoppingCart.map((listing) => (
+                    <ListItem key={listing.name} sx={{ py: 1, px: 0 }}>
+                        <ListItemText primary={listing.productDTO.productName + " x "+listing.quantity} secondary={listing.desc} />
+                        <Typography variant="body2">{listing.price* listing.quantity}</Typography>
                     </ListItem>
                 ))}
                 <ListItem sx={{ py: 1, px: 0 }}>
                     <ListItemText primary="Total" />
                     <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-                        $34.06
+                        {subtotal}
                     </Typography>
                 </ListItem>
             </List>
             <Grid container spacing={2}>
-                <Grid item xs={12} sm={6}>
-                    <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
-                        Shipping
-                    </Typography>
-                    <Typography gutterBottom>John Smith</Typography>
-                    <Typography gutterBottom>{addresses.join(', ')}</Typography>
-                </Grid>
                 <Grid item container direction="column" xs={12} sm={6}>
                     <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
                         Payment details
