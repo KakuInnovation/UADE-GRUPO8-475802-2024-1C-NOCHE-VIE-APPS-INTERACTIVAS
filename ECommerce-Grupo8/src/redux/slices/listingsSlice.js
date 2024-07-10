ae74546cbd43c302d203b70e57ab4aad421b5b34
Agustin <aggarcia997@gmail.com>
2024-07-10 13:22:17 -0300

    Se elimino un slice viejo y se reemplazo por otro
    listingSlice -> Carga todos las publicaciones, publicaciones con stock y demas categorias relacionadas a los productos, al state de redux.

---
+++ ECommerce-Grupo8/src/redux/slices/listingsSlice.js
@@ -0,0 +1,73 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+export const listings = createSlice(
+    {
+        name: 'listings',
+        initialState: {
+            listingsWithStock: [],
+            listings: [],
+            isLoading: false,
+            brands: [],
+            categories: [],
+            difficulties:[],
+            players:[],
+            durations:[],
+            searchedText:"",
+        },
+        reducers: {
+            loadingFetch:(state) => {
+                state.isLoading = true;
+            },
+            fetchListings:(state, action) => {
+                state.listingsWithStock = action.payload;
+                console.log("listings",state.listingsWithStock);
+                state.isLoading = false;
+            },
+            fetchAlListings:(state, action) => {
+                state.listings = action.payload;
+            },
+            fetchBrand:(state, action) => {
+                state.brands = action.payload;
+            },
+            fetchCategory:(state, action) => {
+              state.categories = action.payload;
+            },
+            fetchDifficulties:(state, action) => {
+                state.difficulties = action.payload;
+                state.isLoading = false;
+            },
+            fetchPlayer:(state, action) => {
+                state.players = action.payload;
+                state.isLoading = false;
+            },
+            fetchDurations:(state, action) => {
+                state.durations = action.payload;
+            },
+            fetchSearchedText:(state, action) => {
+                state.searchedText = action.payload;
+            },
+            resetSearchedText:(state, ) => {
+                state.searchedText = "";
+            },
+            failedListingFetch:(state, action) => {
+                state.isLoading = false;
+                state.error = action.payload.error;
+            }
+        }
+    }
+);
+
+export const {
+    loadingFetch,
+    fetchListings,
+    failedListingFetch,
+    fetchBrand,
+    fetchPlayer,
+    fetchDurations,
+    fetchDifficulties,
+    fetchCategory,
+fetchSearchedText,
+resetSearchedText,
+    fetchAlListings
+} = listings.actions;
