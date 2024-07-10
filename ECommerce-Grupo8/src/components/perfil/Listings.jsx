f95a2b68508b68c2c4f8a774649fd390c22ac9de
Agustin <aggarcia997@gmail.com>
2024-07-10 13:56:44 -0300

    Se modifico el componente de publicaciones de usuario en perfil del mismo. Ahora tiene botones que maneja el estado de esa publicacion
    Pausar - publicar - modificar - eliminar

--- ECommerce-Grupo8/src/components/perfil/Listings.jsx
+++ ECommerce-Grupo8/src/components/perfil/Listings.jsx
@@ -14,132 +14,58 @@ import Paper from "@mui/material/Paper";
 import {useState} from "react";
 import '../../assets/fonts/Tisa/Tisa.css'
 import * as React from "react";
+import {sx_grid} from "../../assets/styles/profile/sx_profile_listing_styles.js";
+import {useNavigate} from "react-router-dom";
+import {setUserListings} from "../../redux/slices/userSlice.js";
+import {useDispatch, useSelector} from "react-redux";
+import CreateListingDialog from "./comps/CreateListingDialog.jsx";
+import {deleteUserListing, fetchListingUpdate} from "../../hooks/listing-hooks.js";
 
-const Listings = () => {
+// eslint-disable-next-line react/prop-types
+const Listings = ({open,handleClose,handleOpen, listingsDTO,action,setAction}) => {
+
+
+    const dispatch = useDispatch();
+    const token = useSelector(state => state.auth.token);
     const status =
         [
-            [
+
                 'Publicar',
                 'Pausar',
                 'Modificar',
                 'Eliminar'
-            ],
-            [
-                'Publicar',
-                'Pausar',
-                'Modificar',
-                'Eliminar'
-            ],
-            [
-                'Publicar',
-                'Pausar',
-                'Modificar',
-                'Eliminar'
-            ],
-            [
-                'Publicar',
-                'Pausar',
-                'Modificar',
-                'Eliminar'
-            ]
         ];
-    const sx_grid = {
-        grid:{
-            display:'flex',
-            flexDirection:'column',
-            padding:{xs:'',md:'',lg:'20px'},
 
+    const handleClickButton = (type,item) => {
 
-        },
-        tipoBox:{
-            display:'flex',
-            width:'100%',
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)',
+        switch (type.toUpperCase()) {
+            case "MODIFICAR":
+                dispatch(setUserListings(item));
+                setAction(!action);
+                handleOpen();
 
 
 
-        },
-        tipography:{
-            fontFamily:'Tisa Sans Pro Bold',
-            fontSize:{xs:'',md:'25px'},
-            width:'100%',
-            padding:{xs:'',md:'',lg:'10px'},
-            backgroundColor:'rgba(53,52,52,0.19)',
-        },
-        tableContainer:{
-            display:'flex',
-            backgroundColor:'rgba(53,52,52,0.19)',
-        },
-        table:{
-            display:'flex',
-            justifyContent:'center',
-        },
-        tableBody:{
-            display:'flex',
-            flexDirection:'column',
-            width:'90%',
-            gap:{xs:'',md:'',lg:'10px'},
-            padding:{xs:'',md:'15px',lg:'15px'},
 
-
-        },
-        tableBox:{
-            display:'flex',
-            justifyContent:'center',
-            alignItems:'center',
-            padding:{xs:'',md:'',lg:'10px'},
-            gap:{xs:'',md:'',lg:'10px'},
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)',
-
-            borderBottomLeftRadius:'10px',
-            borderBottomRightRadius:'10px',
-            borderLeftRadius:'10px',
-            borderRightRadius:'10px',
-            borderTopLeftRadius:'10px',
-            borderTopRightRadius:'10px',
-
-
-        },
-        boxTypo:{
-            fontFamily:'Tisa Sans Pro Bold',
-            backgroundColor:'#cfa5a5',
-            color:'#fff',
-            height:'100%',
-            display:'flex',
-            justifyContent:'center',
-            alignItems:'center',
-            padding:{xs:'',md:'',lg:'10px'},
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)',
-            borderBottomLeftRadius:'10px',
-            borderBottomRightRadius:'10px',
-            borderLeftRadius:'10px',
-            borderRightRadius:'10px',
-            borderTopLeftRadius:'10px',
-            borderTopRightRadius:'10px',
-        },
-        buttonBox:{
-            display:'flex',
-            justifyContent:'center',
-            alignItems: 'center',
-            backgroundColor: 'white',
-            gap:{xs:'',md:'',lg:'10px'},
-            padding:{xs:'',md:'',lg:'4px'},
-            width:'80%',
-            border:'2px solid grey',
-            borderBottomLeftRadius:'10px',
-            borderBottomRightRadius:'10px',
-            borderLeftRadius:'10px',
-            borderRightRadius:'10px',
-            borderTopLeftRadius:'10px',
-            borderTopRightRadius:'10px',
-        },
-        button:{
-            display:'flex',
-            gap:{xs:'',md:'',lg:'10px'},
-            padding:{xs:'',md:'',lg:'5px'},
-
+                break;
+            case "PAUSAR":
+                item.listingState =false;
+                fetchListingUpdate(item,token);
+                setAction(!action);
+                break;
+            case "PUBLICAR":
+                item.listingState = true;
+                fetchListingUpdate(item,token);
+                setAction(!action);
+                break;
+            case "ELIMINAR":
+                deleteUserListing(item.listingId,token);
+                setAction(!action);
+                break;
         }
     }
+
+
 
     return(
 
@@ -159,14 +85,15 @@ const Listings = () => {
                 <TableContainer component={Paper} sx={{...sx_grid.tableContainer}}>
                     <Table aria-label="simple table" sx={{...sx_grid.table}}>
                         <TableBody sx={{...sx_grid.tableBody}}>
-                            {status.map((item, index ) => (
+                            {/* eslint-disable-next-line react/prop-types */}
+                            {listingsDTO.map((item, index ) => (
                                 <Box
                                     key={index}
                                     sx={{...sx_grid.tableBox}}>
 
                                     <Typography
                                         sx={{...sx_grid.boxTypo}}>
-                                        Publicacion {index+1/*aca van los id de publicacioens*/}
+                                        Publicacion {item.listingId/*aca van los id de publicacioens*/}
 
                                     </Typography>
                                     <Box
@@ -175,16 +102,24 @@ const Listings = () => {
                                         <TableRow
                                             key={index}
                                             sx={{...sx_grid.button}}>{
-                                            item.map((button, key) => (
+                                            status.map((button, key) => (
                                                 <Button
                                                     key={key}
                                                     style={{
-                                                        backgroundColor:'#cfa5a5',
+
                                                         color:'#fff',
                                                         fontFamily:'Tisa Sans Pro Bold',
                                                         boxShadow: '0 0 10px rgba(0, 0, 0, 0.7)',
-                                                    }}
 
+                                                    }}
+                                                    onClick={()=> handleClickButton (button,item)}
+                                                    disabled={item.listingState === false && button !== "Publicar"}
+                                                    sx={{backgroundColor: button !=="Eliminar"?'#cfa5a5':'#a13434' , filter : item.listingState === false && button !== "Publicar" ?"brightness(50%)" : "brightness(100%)",
+                                                            '&:hover': {
+                                                        backgroundColor: button !== "Eliminar" ? '#c59895' : '#703d3d',
+                                                    },
+
+                                                }}
                                                 >
                                                     {button}
                                                 </Button>
