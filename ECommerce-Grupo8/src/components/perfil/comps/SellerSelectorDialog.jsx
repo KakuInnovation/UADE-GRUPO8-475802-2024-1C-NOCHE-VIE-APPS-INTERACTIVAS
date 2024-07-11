9479581244ebe006459f4116ca440727312b5ba1
Agustin <aggarcia997@gmail.com>
2024-07-11 00:29:23 -0300

    Se actualizaron algunas variables para el acceso a los datos del usuario.

--- ECommerce-Grupo8/src/components/perfil/comps/SellerSelectorDialog.jsx
+++ ECommerce-Grupo8/src/components/perfil/comps/SellerSelectorDialog.jsx
@@ -76,6 +76,7 @@ const SellerSelectorDialog = ({open,handleClose}) => {
     const handleChangeRole = () => {
         if(isChecked){
             updateUserRoleToSeller(email, token)
+            sessionStorage.setItem('rol','SELLER')
         }
     }
 
